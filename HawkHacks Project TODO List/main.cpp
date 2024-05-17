f1b6e300ca5c38d735c7a01a3d8e08b2b5b9c135
Ruiqi Tang <tangricky001@gmail.com>
2024-05-17 19:12:50 -0400

    Added some trial code in main.cpp

---
+++ HawkHacks Project TODO List/main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<algorithm>
+
+using namespace std;
+
+
+
+int main()
+{
+	cout << "Hello Ricky" << endl;
+
+	system("pause");
+	return 0;
+}
