32313f6faeab201ce3079d0c21ec07e428bdf9d9
Ruiqi Tang <tangricky001@gmail.com>
2024-05-17 21:45:59 -0400

    added the header for general class of to do list

---
+++ HawkHacks Project TODO List/ToDoList.h
@@ -0,0 +1,26 @@
+#pragma once
+
+// include the necessary libraries
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include<map>
+
+//namespace
+
+using namespace std;
+
+//Constants
+
+#define STAR "★"
+#define NOMRAL "●"
+
+//Classes
+
+class ToDoList
+{
+public:
+
+};
